beeb811d732ed33aa15d4d8c459caebe5384a484
madara07 <madara07@madara07.madara07>
2026-01-08 09:02:16 +0530

    Add /health endpoint

--- user_chat/src/main.rs
+++ user_chat/src/main.rs
@@ -4,11 +4,19 @@ use actix_web::{get, App, HttpServer, Responder};
 async fn index() -> impl Responder {
     "Actix-web running"
 }
+#[get("/health")]
+async fn health() -> impl Responder {
+    "OK"
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(health)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
