7993471dc8a36ea49bb21a4f89054cf2cef608db
madara07 <madara07@madara07.madara07>
2026-01-08 09:08:53 +0530

    Add greet user endpoint

--- user_chat/src/main.rs
+++ user_chat/src/main.rs
@@ -9,12 +9,20 @@ async fn health() -> impl Responder {
     "OK"
 }
 
+use actix_web::web;
+
+#[get("/greet/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello, {}!", name)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(health)
+            .service(greet)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
