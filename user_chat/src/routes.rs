e3187d05989bfee76263a90d8bf34291847e5105
madara07 <madara07@madara07.madara07>
2026-01-12 13:58:53 +0530

    Inserted the html and css files

--- user_chat/src/routes.rs
+++ user_chat/src/routes.rs
@@ -7,5 +7,8 @@ use crate::handlers;
 pub fn config_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(handlers::index))
 	.route("/login",web::get().to(handlers::login))
-	.route("/register",web::get().to(handlers::register));
+	.route("/register",web::get().to(handlers::register))
+	.route("/superuser",web::get().to(handlers::superuser))
+	.route("/users",web::get().to(handlers::users))
+	.route("/ws_chat",web::get().to(handlers::ws_chat));
 }
